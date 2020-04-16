aefba4621179214f8f7aa008d6fc47f924dff2a5
joaobagas <joaobagas@users.noreply.github.com>
2020-04-16 14:13:10 +0200

    I think everything is organised.

--- Client/src/ClientMain.java
+++ Client/src/ClientMain.java
@@ -1,2 +1,11 @@
-public class ClientMain {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
--- Server/src/ServerMain.java
+++ Server/src/ServerMain.java
@@ -1,2 +1,5 @@
 public class ServerMain {
+    public static void main(String[] args) {
+
+    }
 }
