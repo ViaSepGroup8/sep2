b7db1cbcee4ad401d874df1311e1cbf5cee2cc71
joaobagas <joaobagas@users.noreply.github.com>
2020-04-18 14:46:02 +0200

    Tried to implement some stuff on the model but I don't know how to change views from the model

--- Client/src/model/WarehouseClientModelManager.java
+++ Client/src/model/WarehouseClientModelManager.java
@@ -11,18 +11,23 @@ public class WarehouseClientModelManager implements WarehouseClientModel {
             case 0:
                 System.out.println("Impossible to login, try again!");
                 //No profession (it will give and error message)
+                break;
             case 1:
                 System.out.println("Logged in as administrator!");
                 //Will open admin view
+                break;
             case 2:
                 System.out.println("Logged in as customer!");
                 //Will open customer view
+                break;
             case 3:
                 System.out.println("Logged in as picker!");
                 //Will open picker view
+                break;
             case 4:
                 System.out.println("Logged in as driver!");
                 //Will open driver view
+                break;
         }
     }
 
