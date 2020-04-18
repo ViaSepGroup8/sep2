76ded01f0e9266dd00b89d3db9556b2ec47470d1
joaobagas <joaobagas@users.noreply.github.com>
2020-04-18 14:44:44 +0200

    Tried to implement some stuff on the model but I don't know how to change views from the model

--- Client/src/model/WarehouseClientModel.java
+++ Client/src/model/WarehouseClientModel.java
@@ -4,4 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface WarehouseClientModel extends Remote {
+    void answer(int profession);
 }
--- Client/src/model/WarehouseClientModelManager.java
+++ Client/src/model/WarehouseClientModelManager.java
@@ -3,7 +3,28 @@ package model;
 public class WarehouseClientModelManager implements WarehouseClientModel {
 
     //Login
-    public void login(String username, String password) { System.out.println("Logging in!"); }
+    public void login(String username, String password) { System.out.println("Logging in..."); }
+
+    @Override
+    public void answer(int profession) {
+        switch (profession) {
+            case 0:
+                System.out.println("Impossible to login, try again!");
+                //No profession (it will give and error message)
+            case 1:
+                System.out.println("Logged in as administrator!");
+                //Will open admin view
+            case 2:
+                System.out.println("Logged in as customer!");
+                //Will open customer view
+            case 3:
+                System.out.println("Logged in as picker!");
+                //Will open picker view
+            case 4:
+                System.out.println("Logged in as driver!");
+                //Will open driver view
+        }
+    }
 
     //Administrator
 
